06fd92d0b8075985534418b87ef8afee20e506ed
Robin Kavenius <robin.kavenius@yh.nackademin.se>
2024-04-02 09:21:03 +0200

    close open stringt if

--- stringt.py
+++ stringt.py
@@ -17,13 +17,4 @@ def stringt(*args, end="\n", sep=" ") -> str:
 
 
 if __name__ == "__main__":
-    # Här kan du skriva testkod för din funktion. Denna körs endast när du kör
-    # filen direkt, och inte när du importerar den som modul i en annan fil.
-    # Koden importeras som en modul av autograding-funktionen för att utföra ett
-    # "smoke test" av din funktion, så det är viktigt att din kod inte kör något
-    # utanför denna if-sats.
-    #
-    # Exempel:
-    #
-    # print(funktionsnamn("hejsan", 99))
-    # print(funktionsnamn([19, 22, 31, 29, 1])
+    pass
